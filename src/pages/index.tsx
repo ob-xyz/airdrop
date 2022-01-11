06ae5176110c3fd4195cd708f730424106618fc0
wn <csignore@whril.com>
2022-01-11 10:56:01 -0500

    uiux update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,81 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>airdrop</title>
+        <title>Airdrop</title>
         <meta name="description" content="airdrop.is" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className={styles.main}>
         <div className={styles.s1}>
-            <h1>Airdrop</h1>
+        <h2>A simple way to send crypto</h2>
+        <div className={styles.batchimgs}>
+          <div className={styles.img}>
+          <Image
+              src="/assets/img/crypto-logos/btc-logo.png"
+              alt="bitcoin logo"
+              width={75}
+              height={75}
+          />
+          </div>
+            <div className={styles.img}>
+            <Image
+              src="/assets/img/crypto-logos/eth-logo.png"
+              alt="ethereum logo"
+              width={75}
+              height={75}
+          />
+            </div>
+            <div className={styles.img}>
+            <Image
+              src="/assets/img/crypto-logos/ava-logo.png"
+              alt="solana logo"
+              width={75}
+              height={75}
+          />
+            </div>
+            <div className={styles.img}>
+            <Image
+              src="/assets/img/crypto-logos/yfi-logo.png"
+              alt="avalanche logo"
+              width={75}
+              height={75}
+          />
+          </div>
+            <div className={styles.img}>
+            <Image
+              src="/assets/img/crypto-logos/doge-logo.png"
+              alt="yfi logo"
+              width={75}
+              height={90}
+          />
+            </div>
+            <div className={styles.img}>
+            <Image
+              src="/assets/img/crypto-logos/sol-logo.png"
+              alt="cordano logo"
+              width={75}
+              height={75}
+          />
+            </div>
+            <div className={styles.img}>
+            <Image
+              src="/assets/img/crypto-logos/polygon-logo.png"
+              alt="doge logo"
+              width={75}
+              height={75}
+          />
+            </div>
+            <div className={styles.img}>
+              <Image
+                src="/assets/img/crypto-logos/cordano-logo.png"
+                alt="polygon logo"
+                width={75}
+                height={75}
+            />
+            </div>
         </div>
-        <h2>Payment processing for crypto punks</h2>
+        </div>
       </main>
     </div>
   )
--- src/shared/components/elements/header.tsx
+++ src/shared/components/elements/header.tsx
@@ -6,16 +6,17 @@ import styles from '../../../styles/scss/Header.module.scss'
 const Header: NextPage = () => {
   return (
         <header className={styles.header}>
-            <Link href="/">
-                <a className={styles.imglogo}>
-                <Image
-                    src="/assets/img/logo/airdrop-logoyellow.png"
-                    alt="airdrop logo"
-                    width={55}
-                    height={65}
-                />
-                </a>
-            </Link>
+                <Link href="/">
+                    <a className={styles.subheader}>
+                    <Image
+                        src="/assets/img/logo/airdrop-logoyellow.png"
+                        alt="airdrop logo"
+                        width={40}
+                        height={50}
+                    />
+                    <h1>airdrop</h1>
+                    </a>
+                </Link>
         </header>
   )
 }
