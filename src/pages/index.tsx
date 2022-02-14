dc364fcd048db6f337d235dc89b6caf5ccfab355
wn <csignore@whril.com>
2022-02-13 23:10:26 -0500

    copy-update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,23 +8,23 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Airdrop | Get paid to write stories</title>
-        <meta name="description" content="Where writers tell stories."/>
+        <title>Airdrop | Drop topics you love.</title>
+        <meta name="description" content="Start conversations about topics you love."/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className={styles.main}>
         <div className={styles.s1}>
           <div className={styles.text}>
-            <h2>Where writers tell stories</h2>
-            <h4>Create a newsletter today.</h4>
+            <h2>Drop topics you love 🖤</h2>
+            <h4>Start conversations about topics you love.</h4>
             <h3>Launching 2022</h3>
           </div>
         <div className={styles.batchimgs}>
           <div className={styles.img}>
           <Image
-              src="/assets/img/home/walking-bg.jpg"
-              alt="entrepreneurs walking"
+              src="/assets/img/home/social.webp"
+              alt="social eggs"
               width={1000}
               height={700}
               priority={true}
