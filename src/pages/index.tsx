a28bc65f52619a658719a5c1930c3783e50279eb
wn <csignore@whril.com>
2022-02-22 12:59:01 -0500

    copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
           <div className={styles.text}>
             <h3>Lifeblood for Founders</h3>
             <p>Inspired by Founders Fund, we believe founders are like gods reinventing the world.</p>
-            <p>Successful founders get three things right: make a great product, hire an amazing team, and set the vision. But the best founders? They use every ounce of capital available to constantly perfect their business.</p>
+            <p>Successful founders get three things right — they make a great product, hire an amazing team, and set the vision. But the best founders? They use every ounce of capital available to constantly perfect their business.</p>
             <p>Our goal is to instantly airdrop capital to founders reinventing the world. And we do it by partnering with capital allocators and making software that works. If your startup is growing like a weed, please consider raising your Series A on airdrop.</p>
             <p>Contact chris@airdrop.is or reach by dm.</p>
           </div>
