798c13277b89f6f356b29ca826c410b124e8aeb6
wn <csignore@whril.com>
2022-02-22 11:00:21 -0500

    copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>airdrop</title>
-        <meta name="description" content="Get money for your startup"/>
+        <meta name="description" content="Instantly get funding for your startup from investors like a16z, Founders Fund, Tiger Global, Social Capital, Coatue, & more!"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
@@ -16,9 +16,10 @@ const Home: NextPage = () => {
         <div className={styles.s1}>
           <div className={styles.text}>
             <h3>Lifeblood for Founders</h3>
-            <p>Inspired by Founders Fund, we believe founders are like gods reinventing the world. But they can't grow an amazing team or build cool products without capital.</p>
-            <p>Our goal is to instantly airdrop capital to founders reinventing the world. We do it by partnering with the best capital allocators and building software that works. If your startup is growing like a weed, please consider raising your Series A on airdrop.</p>
-            <p>Contact chris@airdrop.is</p>
+            <p>Inspired by Founders Fund, we believe founders are like gods reinventing the world.</p>
+            <p>To be a successful founder you need to do three things right: make a great product, hire an amazing team, and set the vision. Each of these steps is easier to do with capital.</p>
+            <p>Our goal is to instantly airdrop capital to founders reinventing the world. We do it by partnering with the best capital allocators. And making software that works. If your startup is growing like a weed, please consider raising your Series A on airdrop.</p>
+            <p>Contact: chris@airdrop.is</p>
           </div>
         </div>
       </main>
