0c0200d1181c2bd49c11b2e1bfd08e561ffe44e2
wn <csignore@whril.com>
2022-06-10 02:29:22 -0400

    x

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,16 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Airdrop® | Host virtual events like a rockstar</title>
-        <meta name="description" content="Instantly stream your company trade show, conference, summit, presentation, or event."/>
+        <title>Airdrop.is - The Best Sports Betting Platform</title>
+        <meta name="description" content="Airdrop is the ultimate sports betting experience. Use Apple Pay, Google Pay and more to deposit bitcoin and place your bet."/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className={styles.main}>
         <div className={styles.s1}>
           <div className={styles.rainbowbox}>
-            <h1>airdrop</h1>
-            <p>Reimagining events. Everywhere.</p>
+            <h1>Airdrop</h1>
+            <p>Bet on what you know, and love.</p>
           </div>
         </div>
       </main>
