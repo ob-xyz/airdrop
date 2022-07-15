f9392f46b4daf620caca14589a7b4c14a2472d54
wn <csignore@whril.com>
2022-07-15 14:12:17 -0400

    copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,14 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>Airdrop - Live a life of luxury</title>
-        <meta name="description" content="Airdrop is an online retailer of the best brands, products, and collections. Shop for Fashion, Cookwear, Jewelry & more. Try Airdrop for free."/>
+        <meta name="description" content="Airdrop is where the remote work revolution begins. Live in nicely styled condos with hotel amenities. Book your airdrop today!"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className={styles.main}>
         <div className={styles.s1}>
             <h1>Live a life of luxury</h1>
-            <p>Shop the best Shopify brands.</p>
+            <p>Nicely styled condos with hotel amenities</p>
         </div>
       </main>
     </div>
