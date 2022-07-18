fbb8de4d98fd6ebb18124ece4a7e59a9e7d6a6ef
wn <csignore@whril.com>
2022-07-18 11:15:56 -0400

    copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,15 +7,15 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Airdrop - Live a life of luxury</title>
-        <meta name="description" content="Airdrop is where the remote work revolution begins. Live in fully furnished condo with hotel amenities. Book your airdrop today!"/>
+        <title>Airdrop - Work remote, ive anywhere</title>
+        <meta name="description" content="Work remote. Live anywhere. Every airdrop is fully furnished and includes hotel amenities like daily cleaning, laundry service, and more. Book your airdrop today!"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className={styles.main}>
         <div className={styles.s1}>
-            <h1>Live a life of luxury</h1>
-            <p>Fully furnished condos with hotel amenities</p>
+            <h1>Work remote, live anywhere.</h1>
+            <p>Book a fully furnished condo with hotel amenities</p>
         </div>
       </main>
     </div>
